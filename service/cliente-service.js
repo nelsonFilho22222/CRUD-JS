1d4ea3574c0361dc4cfa3dc26587d457e7701b8e
nelsonFiho22222 <nelsoncamarafilho@gmail.com>
2024-09-02 14:22:23 -0300

    save

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,10 +1,12 @@
 const http = new XMLHttpRequest();
 
 const criaNovaLista = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr');
-    const conteudo = `
+    const promise = new Promise((resolve, reject) => {
+
+        const linhaNovoCliente = document.createElement('tr');
+        linhaNovoCliente.innerHTML = `
         <td class="td" data-td>${nome}</td>
-        <td>${email}</td>
+        <td>${email}</td>  
         <td>
         <ul class="tabela__botoes-controle">
             <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
@@ -14,26 +16,33 @@ const criaNovaLista = (nome, email) => {
         </ul>
         </td>
     `;
-    linhaNovoCliente.innerHTML = conteudo;
     return linhaNovoCliente;
-};
-
-const tabela = document.querySelector('[data-tabela]');
+}
 
 http.open("GET", "http://localhost:3000/profile");
 http.send();
 
-http.onload = () => {
-    if (http.status === 200) {
-        const data = JSON.parse(http.responseText); // Parse da resposta JSON
-        data.forEach(elemento => {
-            tabela.appendChild(criaNovaLista(elemento.nome, elemento.email));
-        });
-    } else {
-        console.error("Erro ao carregar os dados:", http.statusText);
-    }
-};
+
+
+
+    http.onload = () => {
+        if (http.status === 200) {
+            const data = JSON.parse(http.response);
+            data.forEach(elemento => {
+                tabela.appendChild(criaNovaLista(elemento.nome, elemento.email));
+            });
+        } else {
+            console.error("Erro ao carregar os dados:", http.statusText);
+        }
+    };
+    return promise;
+
+})
+
+
+const tabela = document.querySelector('[data-tabela]');
 
 http.onerror = () => {
     console.error("Erro ao fazer a requisição.");
 };
+
